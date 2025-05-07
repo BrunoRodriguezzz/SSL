913ba1f3a28ed18d6e6f87551977ada69ac6c47f
BrunoRodriguezzz <brrodriguez@frba.utn.edu.ar>
2025-05-07 12:25:07 -0300

    TP 2 hecho

---
+++ DD-FasesErrores/hello4.c
@@ -0,0 +1,5 @@
+int prontf(const char * restrict s, ...);
+int main(void){
+ int i=42;
+ prontf("La respuesta es %d\n");
+}
---
+++ DD-FasesErrores/hello8.c
@@ -0,0 +1,7 @@
+void prontf(const char* s, int i);
+
+int main(void){
+    int i=42;
+    prontf("La respuesta es %d\n", i);
+   }
+   
